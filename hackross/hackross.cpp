6f319de0e6b0df808de090de74ca811bb8e1f602
Azeem Bande-Ali <a.bandeali@gmail.com>
2019-04-17 19:01:08 -0400

    Builds

---
+++ hackross/hackross.cpp
@@ -0,0 +1,16 @@
+﻿// hackross.cpp : Defines the entry point for the application.
+//
+
+#include "hackross.h"
+
+extern "C" {
+#include <z3.h>
+}
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
